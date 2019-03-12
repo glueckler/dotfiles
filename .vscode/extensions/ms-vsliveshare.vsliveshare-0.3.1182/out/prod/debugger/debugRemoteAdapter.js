efbc8242a932a81a58c5e4884f4954cc108a153d
Dean Glueckler <shiftbean@Deans-MacBook-Pro.local>
2019-03-12 13:07:48 -0700

    deleted .vscode

--- .vscode/extensions/ms-vsliveshare.vsliveshare-0.3.1182/out/prod/debugger/debugRemoteAdapter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-Object.defineProperty(exports, "__esModule", { value: true });
-const remoteDebug_1 = require("./remoteDebug");
-remoteDebug_1.RemoteDebugSession.run(remoteDebug_1.RemoteDebugSession);
-
-//# sourceMappingURL=debugRemoteAdapter.js.map
